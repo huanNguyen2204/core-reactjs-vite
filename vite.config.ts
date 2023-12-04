d0daf42730d7846e317f5a8d3f569068e291baee
huanNguyen2204 <huan.nc2204@sinhvien.hoasen.edu.vn>
2023-12-05 01:28:53 +0700

    add testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import babel from 'vite-plugin-babel';
-import mkcert from 'vite-plugin-mkcert'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import babel from "vite-plugin-babel";
+import mkcert from "vite-plugin-mkcert";
 
-import path from 'path'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    mkcert(),
-    babel()
-  ],
+  plugins: [react(), mkcert(), babel()],
 
   server: {
     port: 8172,
-    host: true
+    host: true,
   },
 
   resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
-  }
-})
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+});
